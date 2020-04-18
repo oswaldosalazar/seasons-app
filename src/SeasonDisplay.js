442a7fdb1308c25637e8d1d000bcdbb3fb73e28b
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2020-04-18 14:02:00 -0600

    Edit text

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 const seasonConfig = {
   summer: {
-    text: "Let's hit the beach",
+    text: "Let's hit the beach!",
     iconName: 'sun'
   },
   winter: {
-    text: "Burr, it's chilly",
+    text: "Burr, it's chilly!",
     iconName: 'snowflake'
   }
 }
